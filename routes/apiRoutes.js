f3d830f08ee281f010e6058256442b2eaf14bdeb
Baron C <cannonbaron@gmail.com>
2021-07-04 17:27:43 -0500

    adding routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,21 +1,43 @@
-// Routing
-const router = require('express').Router();
+
+const router = require("express").Router();
 const fs = require("fs");
 
+
+// ROUTING
+
 router.get("/notes", function(req, res) {
+
+    // Use fs.readFile to access data in db.json
     fs.readFile("./db/db.json", "utf8", function(err, data) {
+        
+        // Throw error code of there was issue reading db.json
         if (err) throw err;
+
+        // Pass parsed data from db.json as response to be rendered in index.js 
         res.json(JSON.parse(data));
+
     });
+
 });
 
+
+// API POST Request
+
 router.post("/notes", function(req, res) {
+
+    // Use fs.readFile to access data in db.json
     fs.readFile("./db/db.json", "utf8", function(err, data) {
 
+        // Throw error code of there was issue reading db.json
         if (err) throw err;
+
+        // Parse and store db.json data to raw
         let raw = JSON.parse(data);
+
+        // Push user's new note content to raw
         raw.push(req.body);
-        
+
+        // Use fs.writeFile to write raw array data to db.json  
         fs.writeFile("./db/db.json", JSON.stringify(raw), function(err) {
 
             // Throw error code of there was issue writing to db.json
@@ -25,11 +47,59 @@ router.post("/notes", function(req, res) {
             console.log("write success");
 
         });
+
     });
 
     // End response process
     res.end();
 
 });
+
+// // API DELETE Request
+
+// router.delete("/api/notes/:id", function(req, res) {
+
+//     // Store id of user-selected note for deletion 
+//     let id = req.params.id;
+
+//     // Use fs.readFile to access data in db.json
+//     fs.readFile("./db/db.json", "utf8", function(err, data) {
+
+//         // Throw error code of there was issue reading db.json
+//         if (err) throw err;
+
+//         // Parse and store db.json data to raw
+//         let raw = JSON.parse(data);
+
+//         // Loop through the entirity of raw
+//         for (let i = 0; i < raw.length; i++) {
+
+//             // See if the user selected id matches any of the id's in raw
+//             if (id == raw[i].id) {
+
+//                 // If the id's match, splice the indexed note out of raw 
+//                 raw.splice(i,1);
+
+//                 // Use fs.writeFile to write raw array data to db.json  
+//                 fs.writeFile("./db/db.json", JSON.stringify(raw), function(err) {
+
+//                     // Throw error code of there was issue reading db.json
+//                     if (err) throw err;
+
+//                     // log "note deleted" to console/terminal
+//                     console.log("note deleted");
+
+//                 });
+
+//             };
+
+//         };
+
+//     });
+    
+//     // End response process
+//     res.end(); 
+
+// });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 //Dependencies
 const express = require('express');
-const apiRoutes = require('./routes/apiRoutes');
-const htmlRoutes = require('./routes/htmlRoutes');
+const apiRoute = require('./routes/apiRoutes');
+const htmlRoute = require('./routes/htmlRoutes');
 
 //Express Config
 const app = express();
@@ -9,14 +9,14 @@ const PORT = process.env.PORT || 3001;
 
 
 //setup for data parsing 
-app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static("public"));
 
 
 //Router: points our server to a series of routes
-app.use('/api', apiRoutes);
-app.use('/', htmlRoutes);
+app.use("/api", apiRoute);
+app.use("/", htmlRoute);
 
 //Start server
 app.listen(PORT, function() {
